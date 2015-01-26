740e59957003d48de9f1fa75f844d140cde37bea
unknown <X:\The Bat\Data\Mail>
2015-01-26 21:10:33 +0500

    Temp commit

---
+++ tarakan_robot_module.cpp
@@ -0,0 +1,384 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include <winsock2.h>
+#include <ws2bth.h>
+#include <map>
+#include <vector>
+
+#include "../module_headers/module.h"
+#include "../module_headers/robot_module.h"
+
+#include "tarakan_robot_module.h"
+#include "SimpleIni.h"
+
+#pragma comment(lib, "Ws2_32.lib")
+
+typedef ULONGLONG *pbt_addr, *PBT_ADDR;
+typedef std::vector< std::pair< int, int > > universalVec;
+typedef CSimpleIniA::TNamesDepend::const_iterator ini_i;
+
+#define DEFAULT_SOCKET_BUFLEN 512
+#define PATH_TO_CONFIG "robot_modules/tarakan/config.ini"
+
+/* GLOBALS CONFIG */
+const int COUNT_FUNCTIONS = 6;
+const int COUNT_AXIS = 4;
+
+#define DEFINE_ALL_FUNCTIONS \
+	ADD_ROBOT_FUNCTION("moveTo", 2, true)					/*direction, distanse*/\
+	ADD_ROBOT_FUNCTION("rotateTo", 2, false)				/*direction, angle*/\
+	ADD_ROBOT_FUNCTION("moveToByTime", 2, true)				/*direction, time*/\
+	ADD_ROBOT_FUNCTION("rotateToByTime", 2, false)			/*direction, time*/\
+	ADD_ROBOT_FUNCTION("changeLightMode", 3, false)			/*type, strength, period*/\
+	ADD_ROBOT_FUNCTION("getDistanceObstacle", 1, false)		/*direction*/
+
+#define DEFINE_ALL_AXIS \
+	ADD_ROBOT_AXIS("locked", 1, 0)\
+	ADD_ROBOT_AXIS("straight", 1, -1)\
+	ADD_ROBOT_AXIS("rotation", 1, -1)
+
+universalVec vec_rotate, vec_move;
+
+bool pred(const std::pair<int, int> &a, const std::pair<int, int> &b) {
+	return a.first < b.first;
+}
+
+long int getParametrsToTime(int parametr, bool what){
+	universalVec::const_iterator iter_key;
+	universalVec* linkOfaddressMemVec;
+	if (what){ // 1 поворот
+		linkOfaddressMemVec = &vec_rotate;
+	}else{ // 0 движение
+		linkOfaddressMemVec = &vec_move;
+	}
+	int min = 0, max = 0, minValue = 0, maxValue = 0, count = 0;
+	long double y = 0;
+	bool flagEnd = false;
+		for (iter_key = linkOfaddressMemVec->begin(); iter_key != linkOfaddressMemVec->end(); ++iter_key){
+			if (parametr >= iter_key->first){
+				min = iter_key->first;
+				count = iter_key->second;
+			}
+			else{
+				max = iter_key->first;
+				maxValue = iter_key->second;
+				minValue = count;
+				flagEnd = true;
+				break;
+			}
+			count++;
+		}
+		if (!flagEnd){
+			--iter_key;
+			max = iter_key->first;
+			maxValue = iter_key->second;
+			--iter_key;
+			min = iter_key->first;
+			minValue = iter_key->second;
+		}
+
+	y = (((maxValue - minValue)*(parametr - min)) / (max - min)) + minValue;
+
+	return long(std::rint(y));
+}
+
+/////////////////////////////////////////////////
+
+TarakanRobotModule::TarakanRobotModule() {
+	{
+		robot_functions = new FunctionData[COUNT_FUNCTIONS];
+		regval function_id = 0;
+		DEFINE_ALL_FUNCTIONS
+	}
+	{
+		robot_axis = new AxisData[COUNT_AXIS];
+		regval axis_id = 0;
+		DEFINE_ALL_AXIS
+	}
+}
+
+int TarakanRobotModule::init() {
+	srand(time(NULL));
+
+	CSimpleIniA ini;
+	ini.SetMultiKey(true);
+	if (ini.LoadFile(PATH_TO_CONFIG) < 0) {
+		printf("Can't load '%s' file!\n", PATH_TO_CONFIG);
+		return 1;
+	}
+
+	CSimpleIniA::TNamesDepend keys;
+
+	vec_rotate.clear();
+	vec_move.clear();
+		
+	ini.GetAllKeys("rotateTo", keys);
+	for (ini_i ini_key = keys.begin(); ini_key != keys.end(); ++ini_key) {
+		vec_rotate.push_back(
+			std::make_pair(
+				std::stoi(ini_key->pItem, nullptr, 10), 
+				std::stoi(ini.GetValue("rotateTo", ini_key->pItem, NULL), nullptr, 10)
+			)
+		);
+	}
+	keys.clear();
+
+	ini.GetAllKeys("moveTo", keys);
+	for (ini_i ini_key = keys.begin(); ini_key != keys.end(); ++ini_key) {
+		vec_move.push_back(
+			std::make_pair(
+				std::stoi(ini_key->pItem), 
+				std::stoi(ini.GetValue("moveTo", ini_key->pItem, NULL))
+			)
+		);
+	}
+	
+	//сортируем этот вектор по значению
+	std::sort(vec_rotate.begin(), vec_rotate.end(), pred);
+	std::sort(vec_move.begin(), vec_move.end(), pred);
+
+	CSimpleIniA::TNamesDepend values;
+	ini.GetAllValues("connections", "connection", values);
+
+	WSADATA wsd;
+	SOCKET s;
+	SOCKADDR_BTH sab;
+	
+	char recvbuf[DEFAULT_SOCKET_BUFLEN] = "";
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsd) != 0) {
+		printf("Unable to load Winsock! Error code is %d\n", WSAGetLastError());
+		return 1;
+	} else {
+		printf("WSAStartup() is OK, Winsock lib loaded!\n");
+	}
+
+	for (ini_i ini_value = values.begin(); ini_value != values.end(); ++ini_value) {
+		std::string connection(ini_value->pItem);
+
+		try {
+			s = socket(AF_BTH, SOCK_STREAM, BTHPROTO_RFCOMM);
+			
+			if (s == INVALID_SOCKET) {
+				printf("Socket creation failed, error %d\n", WSAGetLastError());
+			} else{
+				printf("socket() looks fine!\n");
+			}
+
+			memset(&sab, 0, sizeof(sab));
+			sab.addressFamily = AF_BTH;
+			sab.btAddr = std::stoll(connection.c_str(), nullptr, 16);
+			sab.serviceClassId = SerialPortServiceClass_UUID;
+			sab.port = BT_PORT_ANY;
+
+			if (connect(s, (SOCKADDR *)&sab, sizeof(sab)) == SOCKET_ERROR) {
+				//This is magic
+				if (connect(s, (SOCKADDR *)&sab, sizeof(sab)) == SOCKET_ERROR) {
+					printf("connect() failed with error code %d\n", WSAGetLastError());
+					closesocket(s);
+					throw std::exception();
+				}
+			}
+
+			printf("connect() should be fine!\n");
+			
+			TarakanRobot *tarakan_robot = new TarakanRobot(s);
+			printf("DLL: connected to %s robot %p\n", connection.c_str(), tarakan_robot);
+			aviable_connections.push_back(tarakan_robot);
+		} catch (...) {
+			printf("Cannot connect to robot with connection: %s\n", connection.c_str());
+		}
+	}
+
+	return 0;
+}
+
+FunctionData* TarakanRobotModule::getFunctions(int *count_functions) {
+	(*count_functions) = COUNT_FUNCTIONS;
+	return robot_functions;
+}
+
+AxisData* TarakanRobotModule::getAxis(int *count_axis) {
+	(*count_axis) = COUNT_AXIS;
+	return robot_axis;
+}
+
+Robot* TarakanRobotModule::robotRequire() {
+	printf("DLL: new robot require\n");
+
+	int count_robots = aviable_connections.size();
+	if (!count_robots){
+		return NULL;
+	}
+
+	int index = rand() % count_robots;
+	int j = 0;
+	for (m_connections_i i = aviable_connections.begin(); i != aviable_connections.end(); ++i) {
+		if ((*i)->isAviable) {
+			if (j == index) {
+				(*i)->isAviable = false;
+				return (*i);
+			}
+			++j;
+		}
+	}
+
+	return NULL;
+}
+
+void TarakanRobotModule::robotFree(Robot *robot) {
+	TarakanRobot *tarakan_robot = reinterpret_cast<TarakanRobot*>(robot);
+
+	for (m_connections_i i = aviable_connections.begin(); i != aviable_connections.end(); ++i) {
+		if ((*i) == tarakan_robot) {
+			printf("DLL: free robot: %p\n", tarakan_robot);
+			tarakan_robot->isAviable = true;
+			break;
+		}
+	}
+}
+
+void TarakanRobotModule::final() {
+	for (m_connections_i i = aviable_connections.begin(); i != aviable_connections.end(); ++i) {
+		closesocket((*i)->getSocket());
+		delete (*i);
+	}
+	aviable_connections.clear();
+
+	vec_rotate.clear();
+	vec_move.clear();
+	
+	WSACleanup();
+}
+
+void TarakanRobotModule::destroy() {
+	delete[] robot_functions;
+	delete[] robot_axis;
+	delete this;
+}
+
+FunctionResult* TarakanRobot::executeFunction(regval command_index, regval *args) {
+	FunctionResult *fr;
+	try {
+		std::string command_for_robot = "";
+		bool need_result = false;
+
+		if ((args[0] != 0) && (args[0] != 1)) {
+			throw;
+		}
+
+		if (
+			(command_index >= 1)
+			&& (command_index <= 4)
+		){
+			if (args[1] < 0) {
+				throw;
+			}
+		}
+
+		if (command_index != ROBOT_COMMAND_INDEX_HAND_CONTROL) {
+			command_for_robot += std::to_string(command_index);
+			command_for_robot += args[0] ? "0" : "1";
+		} else {
+			command_for_robot += "H";
+		}
+
+		switch (command_index) {
+			case 1:	// moveTo
+			case 2: // rotateTo
+				command_for_robot += std::to_string(getParametrsToTime(args[1], 0) * 1000);
+				break;
+			case 3:	// moveToByTime
+			case 4: // rotateToByTime
+				command_for_robot += std::to_string(args[1]);
+				break;
+			case 5: { //changeLightMode
+				if ((args[1] < 0)||(args[1] > 100)) {
+					throw;
+				}
+				if (args[2] < 0) {
+					throw;
+				}
+
+				if (
+					(args[1] >= 10)
+					|| (args[1] < 100)
+				) { 
+					command_for_robot.append(std::to_string(0)); 
+				} else if (args[1] < 10) { 
+					command_for_robot.append(std::to_string(00)); 
+				}
+
+				command_for_robot.append(std::to_string(args[1]));
+				command_for_robot.append(std::to_string(args[2]));
+				break;
+			}
+			case 6: //getDistanceObstacle
+				need_result = true;
+			default: 
+				throw;
+		}
+
+		command_for_robot += "&";
+
+		fd_set readset;
+		FD_ZERO(&readset);
+		FD_SET(socket, &readset);
+
+		send(socket, command_for_robot.c_str(), command_for_robot.length(), 0);
+
+		int result = select(socket, &readset, NULL, NULL, NULL);
+		if (result < 0 && errno != EINTR){
+			printf("Error in select(): %s\n", strerror(errno));
+			throw;
+		}
+		
+		char recvbuf[DEFAULT_SOCKET_BUFLEN] = "";
+		std::string recvstr = "";
+		while (recvstr.find('&') == std::string::npos) {
+			int count_recived = recv(socket, recvbuf, DEFAULT_SOCKET_BUFLEN - 1, 0);
+			recvbuf[count_recived] = 0;
+			recvstr.append(recvbuf);
+		}
+
+		if (recvstr[0] != '0') {
+			throw;
+		}
+
+		fr = new FunctionResult(1, 0);
+
+		if (need_result) {
+			recvstr.erase(0, 1);
+			recvstr.erase(recvstr.find('&'), 1);
+			fr->result = std::stoi(recvstr.c_str());
+		}
+	} catch (...) {
+		fr = new FunctionResult(0);
+	}
+
+	return fr;
+}
+
+void TarakanRobot::axisControl(regval axis_index, regval value) {
+	if (axis_index == 1) {
+		locked = !!value;
+	}
+	if ((!locked)||(axis_index == 1))  {
+		std::string command_for_robot = "";
+		command_for_robot.append(std::to_string(axis_index));
+		command_for_robot.append(std::to_string(value));
+		send(socket, command_for_robot.c_str(), command_for_robot.length(), 0);
+	}
+}
+
+SOCKET TarakanRobot::getSocket() {
+	return socket;
+}
+
+__declspec(dllexport) RobotModule* getRobotModuleObject() {
+	TarakanRobotModule *lrm = new TarakanRobotModule();
+	RobotModule *rm = lrm;
+	return rm;
+}
---
+++ tarakan_robot_module.h
@@ -0,0 +1,51 @@
+#ifndef TARAKAN_ROBOT_MODULE_H
+#define	TARAKAN_ROBOT_MODULE_H
+
+class TarakanRobot : public Robot {
+	public:
+		bool isAviable;
+		bool locked;
+		SOCKET socket;
+	
+		TarakanRobot(SOCKET socket) : isAviable(true), locked(false), socket(socket) {}
+		FunctionResult* executeFunction(regval command_index, regval *args);
+		void axisControl(regval axis_index, regval value);
+
+		SOCKET getSocket();
+		~TarakanRobot() {}
+};
+typedef std::vector<TarakanRobot*> m_connections;
+typedef m_connections::iterator m_connections_i;
+
+class TarakanRobotModule : public RobotModule {
+	m_connections aviable_connections;
+	FunctionData *robot_functions;
+	AxisData *robot_axis;
+
+	public:
+		TarakanRobotModule();
+		int init();
+		FunctionData* getFunctions(int *count_functions);
+		AxisData* getAxis(int *count_axis);
+		Robot* robotRequire();
+		void robotFree(Robot *robot);
+		void final();
+		void destroy();
+		~TarakanRobotModule(){};
+};
+
+#define ADD_ROBOT_FUNCTION(FUNCTION_NAME, COUNT_PARAMS, GIVE_EXCEPTION) \
+	robot_functions[function_id].command_index = function_id + 1; \
+	robot_functions[function_id].count_params = COUNT_PARAMS; \
+	robot_functions[function_id].give_exception = GIVE_EXCEPTION; \
+	robot_functions[function_id].name = FUNCTION_NAME; \
+	function_id++;
+
+#define ADD_ROBOT_AXIS(AXIS_NAME, UPPER_VALUE, LOWER_VALUE) \
+	robot_axis[axis_id].axis_index = axis_id + 1; \
+	robot_axis[axis_id].upper_value = UPPER_VALUE; \
+	robot_axis[axis_id].lower_value = LOWER_VALUE; \
+	robot_axis[axis_id].name = AXIS_NAME; \
+	axis_id++;
+
+#endif	/* TARAKAN_ROBOT_MODULE_H */
