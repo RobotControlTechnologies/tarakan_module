165349a4719002c16c2c1f77ea8339501c1935d8
Mikhail Myasnikov <myasnikov.mike@gmail.com>
2015-04-01 13:11:46 +0400

    Update tarakan_robot_module.h
    Убраны комментарии.

--- tarakan_robot_module.h
+++ tarakan_robot_module.h
@@ -20,7 +20,7 @@ typedef std::vector<TarakanRobot*> m_connections;
 typedef m_connections::iterator m_connections_i;
 
 class TarakanRobotModule : public RobotModule {
-	CRITICAL_SECTION TRM_cs; // Критическая секция
+	CRITICAL_SECTION TRM_cs;
 	m_connections aviable_connections;
 	FunctionData **robot_functions;
 	AxisData **robot_axis;
