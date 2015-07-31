636b54353da7916147fa2c8a5a93994ba588fa1b
m79lol <mikhail@tulkin.name>
2015-07-31 11:19:07 +0500

    Formatted to google style code.

--- tarakan_robot_module.h
+++ tarakan_robot_module.h
@@ -1,95 +1,102 @@
 #ifndef TARAKAN_ROBOT_MODULE_H
-#define	TARAKAN_ROBOT_MODULE_H
+#define TARAKAN_ROBOT_MODULE_H
 
-typedef std::vector< std::pair< int, int > > universalVec;
+typedef std::vector<std::pair<int, int> > universalVec;
 
 class TarakanRobot : public Robot {
-	protected:
-		bool is_aviable;
-		bool is_locked;
+ protected:
+  bool is_aviable;
+  bool is_locked;
 
 #ifdef _WIN32
-	SOCKET s;
+  SOCKET s;
 #else
-	int s;
+  int s;
 #endif
 
-		std::vector<variable_value> axis_state;
-		std::string connection;
-		std::string calibration;
+  std::vector<variable_value> axis_state;
+  std::string connection;
+  std::string calibration;
 
-		char *uniq_name;
-		colorPrintfRobotVA_t *colorPrintf_p;
-	public:
-		universalVec vec_rotate, vec_move;
+  char *uniq_name;
+  colorPrintfRobotVA_t *colorPrintf_p;
 
-		std::string sendAndRecv(std::string command_for_robot);
-		long int getParametrsToTime(variable_value parametr, universalVec *linkOfaddressMemVec);
+ public:
+  universalVec vec_rotate, vec_move;
 
-		TarakanRobot(std::string connection, std::string calibration, universalVec vec_rotate, universalVec vec_move,unsigned int uniq_index);
-		void prepare(colorPrintfRobot_t *colorPrintf_p, colorPrintfRobotVA_t *colorPrintfVA_p);
-		bool require();
-		void free();
+  std::string sendAndRecv(std::string command_for_robot);
+  long int getParametrsToTime(variable_value parametr,
+                              universalVec *linkOfaddressMemVec);
 
-		FunctionResult* executeFunction(CommandMode mode, system_value command_index, void **args);
-		void axisControl(system_value axis_index, variable_value value);
+  TarakanRobot(std::string connection, std::string calibration,
+               universalVec vec_rotate, universalVec vec_move,
+               unsigned int uniq_index);
+  void prepare(colorPrintfRobot_t *colorPrintf_p,
+               colorPrintfRobotVA_t *colorPrintfVA_p);
+  bool require();
+  void free();
 
-		~TarakanRobot();
+  FunctionResult *executeFunction(CommandMode mode, system_value command_index,
+                                  void **args);
+  void axisControl(system_value axis_index, variable_value value);
 
-		void colorPrintf(ConsoleColor colors, const char *mask, ...);
+  ~TarakanRobot();
+
+  void colorPrintf(ConsoleColor colors, const char *mask, ...);
 };
-typedef std::vector<TarakanRobot*> m_connections;
+typedef std::vector<TarakanRobot *> m_connections;
 typedef m_connections::iterator m_connections_i;
 
 class TarakanRobotModule : public RobotModule {
 #ifdef _WIN32
-	CRITICAL_SECTION TRM_cs;
+  CRITICAL_SECTION TRM_cs;
 #else
-	pthread_mutex_t TRM_mx;
+  pthread_mutex_t TRM_mx;
 #endif
-	
-	m_connections aviable_connections;
-	FunctionData **robot_functions;
-	AxisData **robot_axis;
-	colorPrintfModuleVA_t *colorPrintf_p;
 
-	public:
-		TarakanRobotModule();
-		//init
-		const char *getUID();
-		void prepare(colorPrintfModule_t *colorPrintf_p, colorPrintfModuleVA_t *colorPrintfVA_p);
+  m_connections aviable_connections;
+  FunctionData **robot_functions;
+  AxisData **robot_axis;
+  colorPrintfModuleVA_t *colorPrintf_p;
 
-		//compiler only
-		FunctionData** getFunctions(unsigned int *count_functions);
-		AxisData** getAxis(unsigned int *count_axis);
-		void *writePC(unsigned int *buffer_length);
+ public:
+  TarakanRobotModule();
+  // init
+  const char *getUID();
+  void prepare(colorPrintfModule_t *colorPrintf_p,
+               colorPrintfModuleVA_t *colorPrintfVA_p);
 
-		//intepreter - devices
-		int init();
-		Robot* robotRequire();
-		void robotFree(Robot *robot);
-		void final();
+  // compiler only
+  FunctionData **getFunctions(unsigned int *count_functions);
+  AxisData **getAxis(unsigned int *count_axis);
+  void *writePC(unsigned int *buffer_length);
 
-		//intepreter - program & lib
-        void readPC(void *buffer, unsigned int buffer_length);
-        
-        //intepreter - program
-        int startProgram(int uniq_index);
-		int endProgram(int uniq_index);
+  // intepreter - devices
+  int init();
+  Robot *robotRequire();
+  void robotFree(Robot *robot);
+  void final();
 
-		//destructor
-		void destroy();
-		~TarakanRobotModule(){};
+  // intepreter - program & lib
+  void readPC(void *buffer, unsigned int buffer_length);
 
-		void colorPrintf(ConsoleColor colors, const char *mask, ...);
+  // intepreter - program
+  int startProgram(int uniq_index);
+  int endProgram(int uniq_index);
+
+  // destructor
+  void destroy();
+  ~TarakanRobotModule(){};
+
+  void colorPrintf(ConsoleColor colors, const char *mask, ...);
 };
 
 #define ADD_ROBOT_AXIS(AXIS_NAME, UPPER_VALUE, LOWER_VALUE) \
-	robot_axis[axis_id] = new AxisData; \
-	robot_axis[axis_id]->axis_index = axis_id + 1; \
-	robot_axis[axis_id]->upper_value = UPPER_VALUE; \
-	robot_axis[axis_id]->lower_value = LOWER_VALUE; \
-	robot_axis[axis_id]->name = AXIS_NAME; \
-	axis_id++;
+  robot_axis[axis_id] = new AxisData;                       \
+  robot_axis[axis_id]->axis_index = axis_id + 1;            \
+  robot_axis[axis_id]->upper_value = UPPER_VALUE;           \
+  robot_axis[axis_id]->lower_value = LOWER_VALUE;           \
+  robot_axis[axis_id]->name = AXIS_NAME;                    \
+  axis_id++;
 
-#endif	/* TARAKAN_ROBOT_MODULE_H */
+#endif /* TARAKAN_ROBOT_MODULE_H */
